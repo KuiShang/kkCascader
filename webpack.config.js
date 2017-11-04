8d76286aeae09d5486ba0b20e290d6c1923c203c
kk <shangkuikui@gomeplus.com>
2017-11-04 16:14:05 +0800

    add npm

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  // entry: './src/main.js',
-  entry: './src/index.js',  //   -打包到npm时候 用这个入口
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
